72c12469a555a9891544066a1532009fe5678df0
sunakarasatish <118924677+sunakarasatish@users.noreply.github.com>
2023-02-06 11:39:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,80 +1,167 @@
 provider "aws" {
-  region = var.region
+  region     = "ap-south-1"
+  access_key = "AKIASOTKCII2JNRJMMCK"
+  secret_key = "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"
 }
 
-data "aws_availability_zones" "available" {}
 
-locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
-}
+resource "aws_vpc" "satya_vpc" {
+  cidr_block       = "10.20.0.0/16"
+ enable_dns_support = true
+ enable_dns_hostnames = true
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
-
-  name = "education-vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
+  tags = {
+    Name = "satish-vpc"
   }
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.5.1"
+resource "aws_subnet" "satyapub_3" {
+  vpc_id     = aws_vpc.satya_vpc.id
+  cidr_block = "10.20.4.0/24"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.24"
-
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
+  tags = {
+    Name = "satish_public3"
   }
+}
+resource "aws_subnet" "satyapub_4" {
+  vpc_id     = aws_vpc.shiva_vpc.id
+  cidr_block = "10.20.5.0/24"
 
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+  tags = {
+    Name = "satish_public5"
+  }
+}
 
-      instance_types = ["t3.small"]
+# Declare the data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+# e.g., Create subnets in the first two available availability zones
+
+resource "aws_subnet" "satyapub_1" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  vpc_id     = aws_vpc.satya_vpc.id
+  cidr_block = "10.20.1.0/24"
+
+  # ...
+}
+
+resource "aws_subnet" "satyapub_2" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+  vpc_id     = aws_vpc.satya_vpc.id
+  cidr_block = "10.20.32.0/24"
+
+  # ...
+}
+#######################################################
+resource "aws_iam_role" "myapp" {
+  name = "eks-cluster-example"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
     }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-  }
+  ]
 }
+POLICY
+}
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.myapp.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.myapp.name
+}
+##########################################################
+resource "aws_eks_cluster" "myapp" {
+  name     = "myapp1"
+  role_arn = aws_iam_role.myapp.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.satyapub_3.id, aws_subnet.satyapub_4.id, aws_subnet.satyapub_1.id, aws_subnet.satyapub_2.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.myapp.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.myapp.certificate_authority[0].data
+}
+##############################################################
+resource "aws_iam_role" "myapp3" {
+  name = "eks-node-group-example1"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.myapp3.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.myapp3.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.myapp3.name
+}
+############################################################
+resource "aws_eks_node_group" "myapp4" {
+  cluster_name    = aws_eks_cluster.myapp.name
+  node_group_name = "example"
+  node_role_arn   = aws_iam_role.myapp3.arn
+  subnet_ids      = [aws_subnet.satyapub_3.id, aws_subnet.satyapub_2.id]
+
+  ami_type = "AL2_x86_64"
+  instance_types = ["t3.small"]
+  
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+
