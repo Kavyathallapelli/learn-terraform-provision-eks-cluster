8d2b539e7426351bac73419bcba87ac2e65b167c
sunakarasatish <118924677+sunakarasatish@users.noreply.github.com>
2023-02-06 11:57:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIASOTKCII2JNRJMMCK"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 
